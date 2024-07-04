4c76129b6ead372258a9916444c2bb67ea8117f3
xiuyiqian <qianxiuyi23@gmail.com>
2024-07-04 10:28:54 -0500

    login controller fixed

--- RealEstate/api/app.js
+++ RealEstate/api/app.js
@@ -2,12 +2,17 @@ import express from 'express';
 import postRoute from './routes/post.js'; 
 import loginoutReg from './routes/auth.js';
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 const app = express();
 
 // Corrected usage of express.json()
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
 // Use the postRoute router for the /api/v3/post path
 app.use('/api/v3/posts', postRoute);
