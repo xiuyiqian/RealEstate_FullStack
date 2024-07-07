7b420cedd6fb90777bfbd76dbc980a70b841f823
xiuyiqian <qianxiuyi23@gmail.com>
2024-07-07 14:24:07 -0500

    upload image function in updateProfile

--- RealEstate/api/controllers/userControl.js
+++ RealEstate/api/controllers/userControl.js
@@ -1,4 +1,5 @@
 import prisma from "../lib/prisma.js"
+import bcrypt from "bcrypt"
 
 export const getUsers = async (req,res) => {
     console.log("it works get Users");
@@ -24,7 +25,7 @@ export const getUser = async (req,res) => {
         });
         res.status(200).json(user);
     }catch(error){
-        console.log(err)
+        console.log(err);
         res.status(500).json(
             {
                 message: "Failed to get the user"
@@ -38,27 +39,57 @@ export const updateUser = async (req,res) => {
     //find out the id from the cookie also compare the id with the cookie id to see
     //if it is the ownwer of the account
     const cookieUserId = req.userId;
-    const reqBody = req.body;
-    console.log(cookieUserId,userId);
+    const {password, profileImage, ...reqBody} = req.body;
+    console.log({password, profileImage, ...reqBody});
+    //console.log(cookieUserId,userId);
     if(userId!=cookieUserId){
         return res.status(403).json(
             {message: "Not Authorized to update"}
         )
     }
+
+    let newEncryptedPassword = null;
+    if (password){
+        newEncryptedPassword = await bcrypt.hash(password,10);
+    }
+    console.log(reqBody);
     try {
         console.log("update user");
         const updatedUser = await prisma.user.update({
           where: { id: userId },
-          data: reqBody
+          data: {
+            ...(newEncryptedPassword && { password:newEncryptedPassword}),
+            ...reqBody,
+            ...(profileImage && {profileImage: profileImage})
+          }
         });
         console.log('User updated successfully:', updatedUser);
-        res.status(200).json(updatedUser);
+        const {password:newPassword, ...infoWithoutpwd} = updatedUser
+        res.status(200).json(infoWithoutpwd);
       } catch (error) {
         console.error('Error updating user:', error);
-        throw error;
+        res.status(500).json({message:"failed to update the user"});
       }
 }
 
 export const deleteUser = async (req,res) => {
+    const userId = req.params.id;
+    const cookieUserId = req.userId;
+    if(userId!=cookieUserId){
+        return res.status(403).json(
+            {message: "Not Authorized to update"}
+        )
+    }
 
+    try {
+        console.log("delete user");
+        await prisma.user.delete({
+          where: { id: userId },
+        });
+        console.log('User deleted successfully');
+        res.status(200).json({message:"delete the user successfully"});
+      } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({message:"failed to delete users"});
+      }
 }
